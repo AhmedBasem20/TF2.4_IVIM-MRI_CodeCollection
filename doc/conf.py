c873d4fe82f5e4a760c2a59edc9c959def873646
AhmedBasem <demhamesab@gmail.com>
2024-03-12 22:09:01 +0200

    Add readthedocs

--- doc/conf.py
+++ doc/conf.py
@@ -34,3 +34,37 @@ html_show_copyright = False
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+
+def run_apidoc(_):
+    """Runs sphinx-apidoc when building the documentation.
+
+    Needs to be done in conf.py in order to include the APIdoc in the
+    build on readthedocs.
+
+    See also https://github.com/rtfd/readthedocs.org/issues/1139
+    """
+    source_dir = os.path.abspath(os.path.dirname(__file__))
+    apidoc_dir = os.path.join(source_dir, '.')
+    package_dir = os.path.join(source_dir, os.pardir, os.pardir, 'src', 'standardized')
+
+    import subprocess
+
+    cmd_path = 'sphinx-apidoc'
+    if hasattr(sys, 'real_prefix'):  # Check to see if we are in a virtualenv
+        # If we are, assemble the path manually
+        cmd_path = os.path.abspath(os.path.join(sys.prefix, 'bin', 'sphinx-apidoc'))
+
+    cli_options = [
+        package_dir,
+        '-o',
+        apidoc_dir,
+        '--private',
+        '--force',
+        '--no-headings',
+        '--module-first',
+        '--no-toc',
+        '--maxdepth',
+        '4',
+    ]
+
+    subprocess.check_call([cmd_path] + cli_options)
