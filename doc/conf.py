cf5f210b04ef9a56e2314ca8b6cf3f40cca2be1b
AhmedBasem <demhamesab@gmail.com>
2024-03-13 07:13:24 +0200

    deploy docs

--- doc/conf.py
+++ doc/conf.py
@@ -12,8 +12,8 @@ import sys
 sys.path.insert(0, os.path.abspath(".."))
 
 project = 'TF2.4 IVIM MRI Code Collection'
-copyright = '2024, Eric Peterson, Oliver Gurney-Champion'
-author = 'Eric Peterson, Oliver Gurney-Champion'
+copyright = '2024, OSIPI'
+author = 'OSIPI team'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -32,39 +32,5 @@ html_show_copyright = False
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
-
-def run_apidoc(_):
-    """Runs sphinx-apidoc when building the documentation.
-
-    Needs to be done in conf.py in order to include the APIdoc in the
-    build on readthedocs.
-
-    See also https://github.com/rtfd/readthedocs.org/issues/1139
-    """
-    source_dir = os.path.abspath(os.path.dirname(__file__))
-    apidoc_dir = os.path.join(source_dir, '.')
-    package_dir = os.path.join(source_dir, os.pardir, os.pardir, 'src', 'standardized')
-
-    import subprocess
-
-    cmd_path = 'sphinx-apidoc'
-    if hasattr(sys, 'real_prefix'):  # Check to see if we are in a virtualenv
-        # If we are, assemble the path manually
-        cmd_path = os.path.abspath(os.path.join(sys.prefix, 'bin', 'sphinx-apidoc'))
-
-    cli_options = [
-        package_dir,
-        '-o',
-        apidoc_dir,
-        '--private',
-        '--force',
-        '--no-headings',
-        '--module-first',
-        '--no-toc',
-        '--maxdepth',
-        '4',
-    ]
-
-    subprocess.check_call([cmd_path] + cli_options)
